a660ac13737401a33e1e43c032d04ce10e2e0ffc
BillNyeTheScienceGuy <BillNyeTheScienceGuy@users.noreply.github.com>
2016-06-17 16:43:46 -0800

    Update shell.h

--- Cpp/shell.h
+++ Cpp/shell.h
@@ -5,9 +5,9 @@
 // Moves elements closer to their general final position with each gap iteration
 void shellSort(int a[], size_t n) {
 	int gap[] = {701, 301, 132, 57, 23, 10, 4, 1};
-    int j, t;
+	int j, t;
 
-    for (int g = 0; g < 8; g++) {
+	for (int g = 0; g < 8; g++) {
 		for (int i = gap[g]; i < n; i += gap[g]) {
 			t = a[i];
 			j = i - gap[g];
@@ -17,7 +17,7 @@ void shellSort(int a[], size_t n) {
 			}
 			a[j + gap[g]] = t;
 		}
-    }
+	}
 }
 
 #endif // _SHELL_H
